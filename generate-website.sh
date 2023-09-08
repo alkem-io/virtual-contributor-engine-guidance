f1323c51449badc4d6c7815350bee720dbd450f7
Neil Smyth <neil@thesmyths.eu>
2023-09-08 21:12:03 +0200

    updated generate website script to run on windows - no dash in filename, absolute path for hugo; additional logging

--- generate-website.sh
+++ generate-website.sh
@@ -3,10 +3,17 @@
 # 1: git repo
 # 2: website destination directory
 # 3: website source directory
+export DESTINATION=/home/alkemio/data/website/generated
+
 echo About to generate website
+echo ...cloning $1 into $3
 rm -r -f $3
 git clone $1 $3
 cd $3
+echo ...cloned, moved to directory: $PWD
+echo ...running hugo into $2
+
 export PATH=$PATH:/usr/local/go/bin:/usr/local
-# note docker on windows requires the bash statement to run this...
-bash hugo --gc -b / -d $2 
+# note use absolute path as docker on windows has issues
+/usr/local/hugo --gc -b / -d $2
+echo ...created
