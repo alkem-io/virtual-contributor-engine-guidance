790b312902e45adff88a32e4c79b776447b7decb
Rene Honig <github@keeners.nl>
2023-12-22 20:03:11 +0100

    start of concurrent processing

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from langchain.callbacks import get_openai_callback
 from langchain.memory import ConversationBufferMemory
-import pika
+#import pika
 import json
 import ai_utils
 import logging
 import sys
 import io
+import asyncio
 import os
 import def_ingest
+import aio_pika
+from aio_pika import connect, RobustConnection, ExchangeType
 from config import config, website_source_path, website_generated_path, website_source_path2, website_generated_path2, vectordb_path, generate_website, local_path, LOG_LEVEL
 
 # configure logging
@@ -38,17 +41,32 @@ logger.info(f"log level app: {LOG_LEVEL}")
 user_data = {}
 user_chain = {}
 
-credentials = pika.PlainCredentials(config['rabbitmq_user'],
-                                    config['rabbitmq_password'])
-parameters = pika.ConnectionParameters(host=config['rabbitmq_host'],
-                                       credentials=credentials)
-logger.info(f"About to connect to RabbitMQ with params {config['rabbitmq_user']}: {config['rabbitmq_host']}\n")
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
+class RabbitMQ:
+    def __init__(self, host, login, password, queue):
+        self.host = host
+        self.login = login
+        self.password = password
+        self.queue = queue
+        self.connection = None
+        self.channel = None
 
-channel.queue_declare(queue=config['rabbitmqrequestqueue'])
+    async def connect(self):
+        self.connection: RobustConnection = await connect(
+            host=self.host,
+            login=self.login,
+            password=self.password
+        )
+        self.channel = await self.connection.channel()
+        await self.channel.declare_queue(self.queue, auto_delete=False)
 
-def query(user_id, query, language_code):
+rabbitmq = RabbitMQ(
+    host=config['rabbitmq_host'],
+    login=config['rabbitmq_user'],
+    password=config['rabbitmq_password'],
+    queue=config['rabbitmqrequestqueue']
+)
+
+async def query(user_id, query, language_code):
     logger.info(f"\nQuery from user {user_id}: {query}\n")
 
     if user_id not in user_data:
@@ -104,43 +122,91 @@ def ingest(source_url, website_repo, destination_path, source_path, source_url2,
 
     return "Ingest function executed"
 
-def on_request(ch, method, props, body):
-    message = json.loads(body)
-    user_id = message['data'].get('userId')
+# Dictionary to keep track of the tasks for each user
+user_tasks = {}
 
-    operation = message['pattern']['cmd']
+async def on_request(message: aio_pika.IncomingMessage):
+    async with message.process():
+        # Parse the message body as JSON
+        body = json.loads(message.body)
 
-    if operation == 'ingest':
-        response = ingest(config['source_website'], config['website_repo'], website_generated_path, website_source_path, config['source_website2'], config['website_repo2'], website_generated_path2, website_source_path2)
-    else:
-        if user_id is None:
-            response = "userId not provided"
+        # Get the user ID from the message body
+        user_id = body['data']['userId']
+
+        logger.debug(f"\nrequest arriving for user id: {user_id}, deciding what to do\n\n") 
+
+        # If there's already a task for this user, wait for it to finish before processing the message
+        if user_id in user_tasks and not user_tasks[user_id].done():
+            logger.debug(f"existing task running for user id: {user_id}, waiting for it to finish first\n\n") 
+            user_tasks[user_id] = asyncio.create_task(process_message_after(user_tasks[user_id], message))
         else:
-            if operation == 'query':
-                if ('question' in message['data']) and ('language' in message['data']):
-                    response = query(user_id, message['data']['question'], message['data']['language'])
-                else:
-                    response = "Query parameter(s) not provided"
-            elif operation == 'reset':
-                response = reset(user_id)
+            # If there's no task for this user, process the message immediately
+            logger.debug(f"no task running for user id: {user_id}, let's move!\n\n") 
+            user_tasks[user_id] = asyncio.create_task(process_message(message))
+
+async def process_message_after(previous_task: asyncio.Task, message: aio_pika.IncomingMessage):
+    # Wait for the previous task to finish
+    await previous_task
+    # Then process the message
+    logger.debug(f"ongoing task finished, no more waiting here!\n\n") 
+    await process_message(message)
+
+
+async def process_message(message: aio_pika.IncomingMessage):
+        body = json.loads(message.body.decode())
+        user_id = body['data'].get('userId')
+
+        operation = body['pattern']['cmd']
+
+        if operation == 'ingest':
+            response = ingest(config['source_website'], config['website_repo'], website_generated_path, website_source_path, config['source_website2'], config['website_repo2'], website_generated_path2, website_source_path2)
+        else:
+            if user_id is None:
+                response = "userId not provided"
             else:
-                response = "Unknown function"
+                if operation == 'query':
+                    if ('question' in body['data']) and ('language' in body['data']):
+                        logger.debug(f"query time for user id: {user_id}, let's call the query() function!\n\n") 
+                        response = await query(user_id, body['data']['question'], body['data']['language'])
+                    else:
+                        response = "Query parameter(s) not provided"
+                elif operation == 'reset':
+                    response = reset(user_id)
+                else:
+                    response = "Unknown function"
 
-    ch.basic_publish(
-        exchange='',
-        routing_key=props.reply_to,
-        properties=pika.BasicProperties(correlation_id=props.correlation_id),
-        body=json.dumps({"operation": "feedback", "result": response})
-    )
+        await rabbitmq.channel.default_exchange.publish(
+            aio_pika.Message(
+                body=json.dumps({"operation": "feedback", "result": response}).encode(),
+                correlation_id=message.correlation_id,
+                reply_to=message.reply_to
+            ),
+            routing_key=message.reply_to
+        )
 
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-    logger.info(f"Response sent for correlation_id: {props.correlation_id}")
-    logger.info(f"Response sent to: {props.reply_to}")
-    logger.info(f"response: {response}")
+        logger.info(f"Response sent for correlation_id: {message.correlation_id}")
+        logger.info(f"Response sent to: {message.reply_to}")
+        logger.info(f"response: {response}")
 
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue=config['rabbitmqrequestqueue'], on_message_callback=on_request)
+async def main():
+    logger.debug(f"main fucntion (re)starting\n")
+    # rabbitmq is an instance of the RabbitMQ class defined earlier
+    await rabbitmq.connect()
 
-logger.info("Waiting for RPC requests")
-channel.start_consuming()
+    await rabbitmq.channel.set_qos(prefetch_count=20)
+    queue = await rabbitmq.channel.declare_queue(rabbitmq.queue, auto_delete=False)
+
+    # Start consuming messages
+    asyncio.create_task(queue.consume(on_request))
+
+    logger.info("Waiting for RPC requests")
+
+    # Create an Event that is never set, and wait for it forever
+    # This will keep the program running indefinitely
+    stop_event = asyncio.Event()
+    await stop_event.wait()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
